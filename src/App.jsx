a48afbd24c6dd441c5cf78fb3288bcbbc4746204
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-15 09:14:29 +0100

    align-items

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ function App() {
 
   return (
     <>
-      
+      <h1>Adekunle Daniel</h1>
+      <p className='pa'>Work in Progress.</p>
+      <p className='para'>This website is the property of Adekunle Daniel  a notable Web3 and blockchain expert all vistors are advised to visit his other social media pages as this space is currently a work in progress</p>
     </>
   )
 }
